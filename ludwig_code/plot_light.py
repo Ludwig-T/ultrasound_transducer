c70c5523fe3d093b8502763f3689fb4e17e54109
Ludwig Tiston <ludwigtiston@gmail.com>
2024-04-12 13:41:33 +0200

    save pickle

--- ludwig_code/plot_light.py
+++ ludwig_code/plot_light.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -6,7 +7,10 @@ def load_data_from_folder(folder_path):
     data = {}  # Dictionary to store data for each coordinate
 
     # Iterate over files in the folder
+    i = 0
     for file_name in os.listdir(folder_path):
+        i += 1
+        print(f"Loading file {i}")
         if file_name.endswith('.csv'):
             # Extract coordinates from the file name
             coordinates = tuple(map(float, file_name.split('_')[:2]))
@@ -20,10 +24,16 @@ def load_data_from_folder(folder_path):
 
             # Store mean voltage for the coordinate
             data[coordinates] = mean_voltage
+    
+    filename = "data.pkl"
+    with open(filename, 'wb') as file:  # 'wb' mode opens the file in binary write mode
+    # Use pickle.dump() to save the dictionary to the file
+        pickle.dump(data, file)
 
     return data
 
 def plot_mean_voltages(data):
+    print("Plotting data")
     # Extract coordinates and mean voltages from the dictionary
     coordinates, mean_voltages = zip(*data.items())
 
@@ -38,7 +48,7 @@ def plot_mean_voltages(data):
     plt.show()
 
 # Folder path containing CSV files
-folder_path = "C:/Users/tiston/code/ludwig_code/raw_data2"
+folder_path = "C:/Users/tiston/code/ludwig_code/raw_data3"
 
 # Load data from folder
 data = load_data_from_folder(folder_path)
