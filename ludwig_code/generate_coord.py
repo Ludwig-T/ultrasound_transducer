151dc95b04748a52ea58fadfad33313b14bd4f2b
Ludwig Tiston <ludwigtiston@gmail.com>
2024-04-19 14:58:09 +0200

    zig zag pattern

--- ludwig_code/generate_coord.py
+++ ludwig_code/generate_coord.py
@@ -15,11 +15,16 @@ def generate_coordinates(specifications, filename):
         step_y_factor = int(1 / step_y)
         step_z_factor = int(1 / step_z)
 
-        for x in range(int(min_x * step_x_factor), int(max_x * step_x_factor) + 1, 1):
-            for y in range(int(min_y * step_y_factor), int(max_y * step_y_factor) + 1, 1):
-                for z in range(int(min_z * step_z_factor), int(max_z * step_z_factor) + 1, 1):
-                    writer.writerow([x / step_x_factor, y / step_y_factor, z / step_z_factor])
-                    size += 1
+        for y in range(int(min_y * step_y_factor), int(max_y * step_y_factor) + 1, 1):
+            for i, x in enumerate(range(int(min_x * step_x_factor), int(max_x * step_x_factor) + 1, 1)):
+                if i % 2 == 1: # Change every other to create zig-zag pattern
+                    for z in range(int(max_z * step_z_factor), int(min_z * step_z_factor) - 1, -1):
+                        writer.writerow([x / step_x_factor, y / step_y_factor, z / step_z_factor])
+                        size += 1
+                else:
+                    for z in range(int(min_z * step_z_factor), int(max_z * step_z_factor) + 1, 1):
+                        writer.writerow([x / step_x_factor, y / step_y_factor, z / step_z_factor])
+                        size += 1
     return size
 
 if __name__ == "__main__":
@@ -29,8 +34,8 @@ if __name__ == "__main__":
         'Y': (0, 1.5, 0.01),   # Hydrophone direction
         'Z': (-0.4, 0.4, 0.02)   # Vertical
     }
-    size = generate_coordinates(specifications, "coordinates_3d.csv")
+    size = generate_coordinates(specifications, "coordinates_2.csv")
     time_per_measurement = 1 # s
     print(f"Total time to complete measurement: {size * time_per_measurement / (60 * 60 * 24)} days.")
-    print(f"Esstimated data size: {size * 8022792 / 1024 ** 3} GB")
+    print(f"Esstimated data size: {size * 3208328 / 1024 ** 3} GB") # Old size 8022792
     print(f"Number of data points: {size}")
