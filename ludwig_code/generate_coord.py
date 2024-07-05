b31a8be775c2d6eb442290d7611f914dd9cc5e58
Ludwig Tiston <ludwigtiston@gmail.com>
2024-07-05 17:20:38 +0200

    fix /0 and add round numbers

--- ludwig_code/generate_coord.py
+++ ludwig_code/generate_coord.py
@@ -32,43 +32,21 @@ def generate_coordinates(specifications, filename):
         min_z, max_z, step_z = specifications.get('Z')
         MIN_x, MAX_x, STEP_x = specifications.get('X_big')
         MIN_z, MAX_z, STEP_z = specifications.get('Z_big')
-        # Convert step sizes to integers by multiplying them by a suitable factor
-        step_x_factor = int(1 / step_x)
-        step_y_factor = int(1 / step_y)
-        step_z_factor = int(1 / step_z)
+        
+        step_x_factor = 1 / step_x
+        step_y_factor = 1 / step_y
+        step_z_factor = 1 / step_z
 
-        """    
-        y = min_y
-        zigzag = True
-        while y <= max_y:
-            x = MIN_x
-            while x <= MAX_x:
-                if min_x - STEP_x <= x < max_x:
-                    x_var_step = step_x
-                else:
-                    x_var_step = STEP_x
-                z = MIN_z
-                while z <= MAX_z:
-                    if min_x - STEP_x <= x < max_x and min_z - STEP_z <= z < max_z:
-                        z_var_step = step_z
-                    else:
-                        z_var_step = STEP_z
-                    writer.writerow([x, y, z])
-                    size += 1
-                    z += z_var_step
-                x += x_var_step
-            y += step_y
-        """
 
         for y in range(int(min_y * step_y_factor), int(max_y * step_y_factor) + 1, 1):
             for i, x in enumerate(range(int(min_x * step_x_factor), int(max_x * step_x_factor) + 1, 1)):
                 if i % 2 == 1: # Change every other to create zig-zag pattern
                     for z in range(int(max_z * step_z_factor), int(min_z * step_z_factor) - 1, -1):
-                        writer.writerow([x / step_x_factor, y / step_y_factor, z / step_z_factor])
+                        writer.writerow([round(x / step_x_factor, 3), round(y / step_y_factor, 3), round(z / step_z_factor, 3)])
                         size += 1
                 else:
                     for z in range(int(min_z * step_z_factor), int(max_z * step_z_factor) + 1, 1):
-                        writer.writerow([x / step_x_factor, y / step_y_factor, z / step_z_factor])
+                        writer.writerow([round(x / step_x_factor, 3), round(y / step_y_factor, 3), round(z / step_z_factor, 3)])
                         size += 1
         
     return size
@@ -77,30 +55,55 @@ if __name__ == "__main__":
 
     specifications_search = {
         #    (min, max, step)
-        'X': (-2, 2, 0.1),  # Horizontal
-        'Y': (0, 0, 0.25),   # Hydrophone direction (should be in [0, inf) ) to go away from hydrophone
-        'Z': (-2, 2, 0.1),   # Vertical
+        'X': (-5, 5, 0.75),  # Horizontal
+        'Y': (0, 30, 3),   # Hydrophone direction (should be in [0, inf) ) to go away from hydrophone
+        'Z': (-5, 5, 0.75),   # Vertical
         "X_big" : (-1, 1, 0.1),
         "Z_big" : (-1, 1, 0.1)
     }
     
     specifications_meas = {
         #    (min, max, step)
-        'X': (-0.5, 0.5, 0.5),  # Horizontal
-        'Y': (0, 6, 0.075),   # Hydrophone direction (should be in [0, inf) ) to go away from hydrophone
-        'Z': (-1.5, 1.5, 0.075),   # Vertical
+        'X': (0, 0, 0.075),  # Horizontal
+        'Y': (0, 30, 0.075),   # Hydrophone direction (should be in [0, inf) ) to go away from hydrophone
+        'Z': (-3, 3, 0.075),   # Vertical
         "X_big" : (-1, 1, 0.1),
         "Z_big" : (-1, 1, 0.1)
     }
+    
+    specifications_meas = {
+        #    (min, max, step)
+        'X': (-3, 3, 0.075),  # Horizontal
+        'Y': (0, 0, 0.15),   # Hydrophone direction (should be in [0, inf) ) to go away from hydrophone
+        'Z': (-3, 3, 0.075),   # Vertical
+        "X_big" : (-1, 1, 0.1),
+        "Z_big" : (-1, 1, 0.1)
+    }
+    
+    """
+    specifications_meas = {
+        #    (min, max, step)
+        'X': (-2.2, 2.2, 0.05),  # Horizontal
+        'Y': (0, 0, 0.075),   # Hydrophone direction (should be in [0, inf) ) to go away from hydrophone
+        'Z': (-2.2, 2.2, 0.05),   # Vertical
+        "X_big" : (-1, 1, 0.1),
+        "Z_big" : (-1, 1, 0.1)
+    }
+    """
+    
     
 
     size_1 = generate_coordinates(specifications_search, "coord_search.csv")
     size_2 = generate_coordinates(specifications_meas, "coord_meas.csv")
     size = size_2
     time_per_measurement = 10859/8829 # s
-    print(f"Time to complete search: {size_1 * time_per_measurement / (60)} minuits.")
-    print(f"Total time to complete measurement: {size * time_per_measurement / (60 * 60)} days.")
-    print(f"Total time including data analyis: {size * (time_per_measurement + 1 / 3.1 ) / (60 * 60 * 24)} days")
-    print(f"Esstimated data size: {size_2 * 3208328 / 1024 ** 3} GB") # Old size 8022792
-    print(f"Number of data points: {size_2}")
-    plot_xz_plane("coordinates_2.csv")
+    
+    time_hour = size * time_per_measurement / (60 * 60)
+    time_day = time_hour / 24
+    # print(f"Time to complete search: {size_1 * time_per_measurement / (60)} minuits.")
+    print(f"Total time to complete measurement: {time_day} days.")
+    print(f"That is {time_hour} hours.")
+    # print(f"Total time including data analyis: {size * (time_per_measurement + 1 / 3.1 ) / (60 * 60 * 24)} days")
+    # print(f"Esstimated data size: {size_2 * 3208328 / 1024 ** 3} GB") # Old size 8022792
+    # print(f"Number of data points: {size_2}")
+    plot_xz_plane("coord_search.csv")
